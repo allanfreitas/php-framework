8635f295b24af68aa4471c7ee7f7b164741506f4
Michal Derlatka <michal.derlatka@allegro.pl>
2012-10-03 15:54:33 +0200

    Initial commit

---
+++ FrontController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: mderlatka
+ * Date: 03.10.12
+ * Time: 15:54
+ * To change this template use File | Settings | File Templates.
+ */
+class FrontController
+{
+
+}
